6875f27f6ccec6acf4113e2b367a16ae9f862189
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-05-29 17:53:03 +0530

    Update AServlet.java

--- Servlet Context/AServlet.java
+++ Servlet Context/AServlet.java
@@ -1,1 +1,23 @@
+package com.servlets;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;    
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class AddServlet extends HttpServlet 
+{
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException
+	{
+		PrintWriter out = res.getWriter(); 
+    
+		ServletContext ctx = getServletContext();
+		String str = ctx.getInitParameter("database");
+    
+    out.println("Param Value: "+ str);
+		
+	}
+}
