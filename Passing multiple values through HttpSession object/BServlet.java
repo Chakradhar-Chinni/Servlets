4aceae01dfc62e1faeff492bf24dbdb555728e4b
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-05-29 14:00:12 +0530

    Create BServlet.java

---
+++ Passing multiple values through HttpSession object/BServlet.java
@@ -0,0 +1,30 @@
+package com.servlets;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class BServlet extends HttpServlet
+{
+	public void service(HttpServletRequest req,HttpServletResponse res)throws  IOException
+	{
+		HttpSession session = req.getSession();
+		PrintWriter out = res.getWriter();      
+		
+		int a = (int) session.getAttribute("a");
+		int b = (int) session.getAttribute("b");
+		int c = (int) session.getAttribute("c");
+                                     
+		out.println("A: "+a);
+		out.println("B: "+b);
+		out.println("C: "+c);
+		//session.removeAttribute("a");
+		int k = (int) session.getAttribute("a");
+		out.println("K: "+k);
+	}
+}
+ 
